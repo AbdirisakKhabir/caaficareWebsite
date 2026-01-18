30b2a0dc34a1833b6a316ccfd8fd002dfb66bf8b
AbdirisakAhmed <khubaro2017@gmail.com>
2026-01-18 18:49:23 +0300

    Update the Header

--- src/pages/DoctorsList.js
+++ src/pages/DoctorsList.js
@@ -143,7 +143,7 @@ const DoctorsList = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-b mt-20 from-gray-50 to-white py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
         <div className="text-center mb-8">
